fadd0c3bc6d5e97d4e3e7df952fa9162a47c96a5
Sayed Sadat <sadatsayedibrahim@gmail.com>
2026-05-05 15:50:58 -0700

    pushed the whole code again

---
+++ README.md
@@ -0,0 +1,20 @@
+
+## Lecture 42
+    - Create a Database & Its user
+        1) Crate a database
+        2) Crate a user for the database
+        3) Add user to the database
+        4) Add permission for the user (We will assign all permissions)
+        4) Import SQL file into the database
+
+    - Upload Files
+
+    - Change Database connection variables
+
+
+
+## Next Class
+    
+    
+
+    
---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+
+$hostname  = "localhost";
+$username = "toptcszk_blog_user";
+$password = "bjp0H4!JkyB=";
+$databaseName = "toptcszk_blog";
+
+//Instance of the class 'mysqli'
+//Its a built in PHP class that we can use to connect to database
+$conn = new mysqli($hostname, $username, $password, $databaseName);
+
+
+//If any error, print msg
+if($conn->connect_error){
+    echo "Connection failed: " . $conn->connect_error;
+}
+
+
+?>
